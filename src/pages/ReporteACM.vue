48b7719a0ee3f770c14552fb10ce25e87d26e3c9
LuisM <118645322+MGLviS@users.noreply.github.com>
2023-12-02 04:40:34 -0500

    Reportes

---
+++ src/pages/ReporteACM.vue
@@ -0,0 +1,47 @@
+<template>
+  <q-page>
+    <q-container class="q-mb-md">
+      <q-select
+        v-model="selectedChart"
+        :options="chartOptions"
+        label="Selecciona un gráfico"
+        emit-value
+        map-options
+      />
+    </q-container>
+
+    <div :class="selectedChart">
+      <component :is="selectedChart" />
+    </div>
+  </q-page>
+</template>
+
+<style scoped>
+/* Ajusta el ancho del gráfico */
+.BarChart {
+  width: 100%; /* Haz ajustes según sea necesario */
+}
+</style>
+
+<script>
+  import BarChart from 'src/components/Reportes/ReporteVehiculo.vue'; // Ajusta la ruta según tu estructura de carpetas
+  import BCFactura from 'src/components/Reportes/ReporteFacturas.vue';
+  export default {
+    data() {
+      return {
+        selectedChart: null,
+        chartOptions: [
+          { label: 'Reporte Vehiculo por Fabrincante', value: 'BarChart' },
+          { label: 'Reporte Montos Facturados', value: 'BCFactura' },
+          // Agrega más opciones según los gráficos disponibles
+        ],
+      };
+    },
+    components: {
+      BarChart,
+      BCFactura
+      // Agrega otros componentes de gráficos aquí según sea necesario
+    },
+  };
+  </script>
+  
