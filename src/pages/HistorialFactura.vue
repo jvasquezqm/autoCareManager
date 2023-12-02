9fbfd4da70e7cc3a2191235135d0a18b1b225947
LuisM <118645322+MGLviS@users.noreply.github.com>
2023-12-02 17:44:43 -0500

    Historial Factura

---
+++ src/pages/HistorialFactura.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="factura-page">
+        <div class="titulo"><h5>HISTORIAL DE FACTURASC</h5></div>
+        <FacturaAdd ref="FacturaAdd" />
+        <br/>
+        <div class="tabla"><FacturaTabla /></div>
+    </div>  
+</template>
+
+
+
+<style>
+
+.factura-page{
+
+    justify-content: space-between;
+    position: sticky;
+}
+.titulo {
+    text-align: center;
+    width: 100%;
+    
+}
+
+.agregar {
+    text-align: left;
+    margin-left: 2%;
+}
+.tabla {
+    text-align: center;
+    width: 100%;
+}
+</style>
+
+
+<script>
+
+import FacturaTabla from 'src/components/Factura/FacturaHistorialTabla.vue';
+
+export default {
+    name: "GestionFactura",
+    components: {
+        FacturaTabla
+    },
+
+    methods: {
+        
+    }
+}
+
+</script>
