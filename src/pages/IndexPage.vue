7bdec40b20b7def9acd1ebe4c7d8e29418a5ac69
LuisM <118645322+MGLviS@users.noreply.github.com>
2023-12-02 18:11:50 -0500

    Mejoras de diseño login

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,7 +2,7 @@
   <q-page class="flex flex-center">
     <img
       alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
+      src="https://i.pinimg.com/originals/fb/4c/ab/fb4cabe2bbe6d6eef9c057b6c94fe27a.jpg"
       style="width: 200px; height: 200px"
     >
   </q-page>
